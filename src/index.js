9ed47897f8162489dcb40c6f1ded105aaea7d66e
harshsahu383 <harshsahu1917@gmail.com>
2025-09-04 00:04:12 +0530

    Issues Fixed : Db

--- src/index.js
+++ src/index.js
@@ -3,8 +3,11 @@ dotenv.config({ path: "./.env" });
 const mongoose = require("mongoose");
 const express = require("express");
 const Listing = require("../models/listing.js")
+const path = require("path");
 const connectDb = require("../db/index.js");
 const app = express();
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"));
 
 app.listen(process.env.PORT, (req,res) => {
     console.log("Server Is Running ");
@@ -13,5 +16,14 @@ app.listen(process.env.PORT, (req,res) => {
 app.get("/", (req,res) => {
     res.send("working ");
 });
-// app.get("/testListing" , ())
+
+// index route
+app.get("/listings", async (req,res) => {
+    const allListings = await Listing.find({});
+    console.log(allListings);
+    
+    res.render("listings/index.ejs", {allListings});
+})
+
+
 
