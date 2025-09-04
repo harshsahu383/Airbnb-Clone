39dab11646c2d30b6e9a23132fb58928df9cf177
harshsahu383 <harshsahu1917@gmail.com>
2025-09-04 16:15:09 +0530

    Create and Add Roite : Completed with Fixed Issues

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ const express = require("express");
 const Listing = require("../models/listing.js")
 const path = require("path");
 const connectDb = require("../db/index.js");
+const { appendFile, appendFileSync } = require("fs");
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../views"));
@@ -25,10 +26,26 @@ app.get("/listings", async (req,res) => {
     res.render("listings/index.ejs", {listings});
 })
 
+//Create Route 
+app.get("/listings/new", (req,res) => {
+    res.render("listings/new.ejs");
+})
+
 // Show route
 app.get("/listings/:id", async (req,res) => {
     let {id} = req.params;
-    const listing = await Listing.findById(id);
-    res.render("listings/show.ejs", {listing});
+    const listings = await Listing.findById(id);
+    res.render("listings/show.ejs", {listings});
 });
 
+app.post("/listings", async (req,res) => {
+    try{
+
+   const newListing =  new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings")}
+    catch(error){
+        console.log(error);
+        
+    }
+});
