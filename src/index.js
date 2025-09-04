9f496101e3369e4adf90ca46fa8c04f09418a17b
harshsahu383 <harshsahu1917@gmail.com>
2025-09-04 14:56:05 +0530

    Created Show/index Route : Backend

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ const connectDb = require("../db/index.js");
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../views"));
+app.use(express.urlencoded({extended:true}));
 
 app.listen(process.env.PORT, (req,res) => {
     console.log("Server Is Running ");
@@ -19,11 +20,15 @@ app.get("/", (req,res) => {
 
 // index route
 app.get("/listings", async (req,res) => {
-    const allListings = await Listing.find({});
-    console.log(allListings);
+    const listings = await Listing.find({});
     
-    res.render("listings/index.ejs", {allListings});
+    res.render("listings/index.ejs", {listings});
 })
 
-
+// Show route
+app.get("/listings/:id", async (req,res) => {
+    let {id} = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", {listing});
+});
 
