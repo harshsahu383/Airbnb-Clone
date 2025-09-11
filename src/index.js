709fc4ea0da23a39636d1a2075f7da5ae7043311
harshsahu383 <harshsahu1917@gmail.com>
2025-09-11 22:50:44 +0530

    Error.ejs Created : Error

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ dotenv.config({ path: "./.env" });
 const mongoose = require("mongoose");
 const express = require("express");
 const Listing = require("../models/listing.js")
+const {lisitngSchema, listingSchema} = require("../src/schema.js")
 const path = require("path");
 const connectDb = require("../db/index.js");
 const wrapAsync = require("../utils/wrapAsync.js")
@@ -10,6 +11,7 @@ const ejsMate = require("ejs-mate");
 const { appendFile, appendFileSync } = require("fs");
 const methodOverride = require("method-override");
 const ExpressError = require("../utils/ExpressError.js");
+const { getDefaultResultOrder } = require("dns");
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "../views"));
@@ -24,6 +26,15 @@ app.listen(process.env.PORT, (req,res) => {
 app.get("/", (req,res) => {
     res.send("working ");
 });
+const validateSchema = (req,res,next) => {
+      const {error} = listingSchema.validate(req.body);
+    if(error){
+        let errMsg = error.details.map((el) => el.message.join(","));
+        throw new ExpressError(404, errMsg);
+    }
+}
+
+
 
 // index route
 app.get("/listings", wrapAsync(async (req,res) => {
@@ -44,10 +55,8 @@ app.get("/listings/:id", wrapAsync(async (req,res) => {
     res.render("listings/show.ejs", {listings});
 }));
 
-app.post("/listings", wrapAsync(async (req,res,next) => {
-    if(! req.body.listing){
-        throw new ExpressError(400,"Please Send Valid Response");
-    }
+app.post("/listings", validateSchema ,wrapAsync(async (req,res,next) => {
+
     const newListing =  new Listing(req.body.listing);
     await newListing.save();
     res.redirect("/listings");
@@ -60,7 +69,7 @@ app.get("/listings/:id/edit", wrapAsync(async (req, res) => {
 }));
 
 //Update Route
-app.put("/listings/:id", wrapAsync(async (req, res) => {
+app.put("/listings/:id", validateSchema, wrapAsync(async (req, res) => {
   let { id } = req.params;
   await Listing.findByIdAndUpdate(id, { ...req.body.listing })
   res.redirect(`/listings/${id}`);
@@ -72,12 +81,11 @@ app.delete("/listings/:id", wrapAsync(async (req, res) => {
   console.log(deletedListing);
   res.redirect("/listings");
 }));
-app.use((req, res, next) => {
-  next(new ExpressError(404, "Page Not Found !"));
-});
-
 // Centralized error handler
+app.use(( req, res, next) => {
+  next()
+});
 app.use((err, req, res, next) => {
   const { statusCode = 500, message = "Something went wrong!" } = err;
-  res.status(statusCode).send(message);
+  res.render("error.ejs" , {err})
 });
